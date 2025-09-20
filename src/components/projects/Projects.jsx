f3b3e83fa73614e6ba9bcce5b1bd1c1f0ceb9b73
Wahid-Haidari <wahidullahhaidari2@gmail.com>
2025-09-19 22:55:31 -0500

    added the demo link for job tracker

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -11,7 +11,7 @@ const data = [
 		image: JOB_TRACKER,
 		title: 'Job Tracker',
 		github: 'https://github.com/Wahid-Haidari/JobTracker',
-		demo: ''
+		demo: 'job-tracker-xkmo.onrender.com/'
 
 	},
 
